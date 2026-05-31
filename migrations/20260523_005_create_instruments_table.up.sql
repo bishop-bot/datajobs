bbf82c1f182e63a113265e66ca12d44a02e15bc0
Albert Chan <atfchan@gmail.com>
2026-05-31 22:15:56 +0800

    chore: update instruments migrations sql

--- migrations/20260523_005_create_instruments_table.up.sql
+++ migrations/20260523_005_create_instruments_table.up.sql
@@ -4,11 +4,13 @@
 CREATE TABLE IF NOT EXISTS instruments (
     id TEXT PRIMARY KEY,
     symbol TEXT NOT NULL,
+    name TEXT NOT NULL,
     publisher TEXT NOT NULL,
     instrument_class TEXT NOT NULL,
     currency TEXT,
     exchange TEXT,
     asset TEXT,
+    security_type TEXT,
     min_lot_size REAL,
     expiration DATETIME,
     max_price_variation REAL,
@@ -21,7 +23,7 @@ CREATE TABLE IF NOT EXISTS instruments (
     maturity_year INTEGER,
     maturity_month INTEGER,
     maturity_day INTEGER,
-    group_ TEXT,
+    "group" TEXT,
     tick_rule TEXT,
     strike_price REAL,
     strike_price_currency TEXT,
@@ -30,6 +32,7 @@ CREATE TABLE IF NOT EXISTS instruments (
 );
 
 CREATE INDEX IF NOT EXISTS idx_instruments_symbol ON instruments(symbol);
+CREATE INDEX IF NOT EXISTS idx_instruments_name ON instruments(name);
 CREATE INDEX IF NOT EXISTS idx_instruments_publisher ON instruments(publisher);
 CREATE INDEX IF NOT EXISTS idx_instruments_exchange ON instruments(exchange);
 CREATE INDEX IF NOT EXISTS idx_instruments_instrument_class ON instruments(instrument_class);
